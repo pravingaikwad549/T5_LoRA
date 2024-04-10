e8098eefd8e4edeec5d1d5e6436b8b6c5a4f6304
Pravin <pravingaikwad549@gmail.com>
2024-04-10 12:48:35 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, render_template, request
+import torch
+from transformers import (T5ForConditionalGeneration, T5Tokenizer)
+from peft import PeftModel
+import collections
+import pandas as pd
+print("Libraries Imported Successfully")
+pd.set_option('display.max_colwidth', None)
+model_id = 't5-large'
+tokenizer = T5Tokenizer.from_pretrained(model_id)
+model = T5ForConditionalGeneration.from_pretrained(model_id)
+
+questions = ['What is the name of the customer?', 'What is the phone number of the customer?', 'What is the address of the customer?', 'Sentiment?', 'Summarize?', 'What is the topic of the text?']
+print("Questions Loaded Successfully")
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    print("Home Page")
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    global questions
+    res = collections.defaultdict(str)
+    # print("type:  #########################",type(request.form['c']))
+    context = request.form['c']
+    name = request.form['name']
+    for i, question in enumerate(questions):
+        input_ids = tokenizer(f" question: {question} context: {context}", return_tensors='pt').input_ids
+        if i == 3:
+            model_senti = PeftModel.from_pretrained(model, "/home/pravin/Desktop/April/7thapril/Sentiment/second_iteration/epoch2", map_location=torch.device('cpu'))
+            output = tokenizer.decode(model_senti.generate(inputs = input_ids)[0], max_length =4, skip_special_tokens=True)
+            res[question] = output
+        elif i == 4:
+            model_summ = PeftModel.from_pretrained(model, "/home/pravin/Desktop/April/8thapril/summarization/epoch1", map_location=torch.device('cpu'))
+            output = tokenizer.decode(model_summ.generate(inputs = input_ids)[0], num_beam= 1, skip_special_tokens=True) 
+            res[question] = output
+        else:
+            model = T5ForConditionalGeneration.from_pretrained(model_id)
+            output = tokenizer.decode(model.generate(inputs = input_ids)[0], max_new_tokens =4, num_beam= 1, skip_special_tokens=True) 
+            res[question] = output
+    res = [{'Question': key, 'Answer': value} for key, value in res.items()]
+    res = pd.DataFrame(res)
+    return render_template('index.html', name = name,  res = res.to_html())
+
+if __name__ == '__main__':
+    app.run(debug=True)
